657c0f310028dd03a6530189baa9cbfc3748d2cf
Milos Vasic <milos85vasic@gmail.com>
2017-03-23 13:48:42 +0100

    Kotlin Factory: Directory structure generation.

--- KotlinFactory/src/main/kotlin/net/milosvasic/factory/kotlin/KotlinProjectFactory.kt
+++ KotlinFactory/src/main/kotlin/net/milosvasic/factory/kotlin/KotlinProjectFactory.kt
@@ -1,7 +1,33 @@
 package net.milosvasic.factory.kotlin
 
+import net.milosvasic.factory.module.Module
 import net.milosvasic.factory.project.ProjectFactory
+import java.io.File
 
 class KotlinProjectFactory(override val workingFolderName: String) : ProjectFactory() {
+
+    override fun getModuleNonJavaDirectories(module: Module, root: File): List<File> {
+        val name = module.name.replace(" ", "_")
+        val localFile = File("${root.absolutePath}${File.separator}$name${File.separator}src")
+        val main = File("${localFile.absolutePath}${File.separator}main")
+        val test = File("${localFile.absolutePath}${File.separator}test")
+        val mainKotlin = File("${main.absolutePath}${File.separator}kotlin")
+        val testKotlin = File("${test.absolutePath}${File.separator}kotlin")
+        val directories = mutableListOf(mainKotlin, testKotlin)
+
+        var packageDirectoryKotlin = mainKotlin.absolutePath
+        var packageDirectoryTestKotlin = testKotlin.absolutePath
+        module.pPackage.split(".").forEach {
+            packageElement ->
+            packageDirectoryKotlin += File.separator + packageElement
+            packageDirectoryTestKotlin += File.separator + packageElement
+        }
+        packageDirectoryKotlin += File.separator + module.group
+        packageDirectoryTestKotlin += File.separator + module.group
+        directories.add(File(packageDirectoryKotlin))
+        directories.add(File(packageDirectoryTestKotlin))
+
+        return directories
+    }
 
 }
